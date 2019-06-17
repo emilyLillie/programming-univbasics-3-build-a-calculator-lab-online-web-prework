3bc58ca5570030a808256d16d978d3d38389ee52
Emily Lillie <emilylillie3@gmail.com>
2019-06-17 21:45:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,21 @@
 def addition(num1, num2)
   sum = num1 + num2
   return sum
-
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  return difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  multply = num1 * num2
+  return multply
 end
 
 def modulo(num1, num2)
